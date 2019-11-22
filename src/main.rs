0435e2c1b3beaecb4718a2fcadba45c3646be822
Hermann Czedik-Eysenberg <hermann.czedik-eysenberg@agfa.com>
2019-11-22 18:28:20 +0100

    Comment Pull Request when it gets opened (work in progress)

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,20 @@
+use actix_web::client::Client;
+use actix_web::middleware::Logger;
+use actix_web::{web, App, Error, HttpServer, Responder};
 use env_logger::Env;
+use futures::future;
+use futures::future::Future;
+use lazy_static::lazy_static;
 use log::info;
+use regex::Regex;
+use serde_json::Value;
 use std::env;
 
-use actix_web::middleware::Logger;
-use actix_web::{web, App, HttpServer, Responder, Result};
+mod types;
+use types::*;
 
-use serde::Deserialize;
-use serde_json::Value;
-
-#[derive(Deserialize)]
-struct Event {
-    #[serde(rename = "eventKey")]
-    event_key: String,
-}
-
-fn index() -> impl Responder {
-    "Hi, I'm the Bitbucket Task Bot!"
-}
-
-fn handle_bitbucket_event(payload: String) -> Result<&'static str> {
-    info!("Received event: {}", payload);
-
-    let v: Value = serde_json::from_str(&payload)?;
-
-    if v["test"].as_bool() == Some(true) {
-        // Bitbucket connection test
-        return Ok("Success");
-    }
-
-    let event: Event = serde_json::from_value(v)?;
-
-    // TODO
-
-    Ok("OK")
+lazy_static! {
+    static ref URL_HOST_REGEX: Regex = Regex::new(r"^(https?://[^/]+/)").unwrap();
 }
 
 fn main() {
@@ -51,4 +33,65 @@ fn main() {
     .unwrap()
     .run()
     .unwrap();
+}
+
+fn index() -> impl Responder {
+    "Hi, I'm the Bitbucket Task Bot!"
+}
+
+fn handle_bitbucket_event(payload: String) -> Box<dyn Future<Item = &'static str, Error = Error>> {
+    info!("Received event: {}", payload);
+
+    let json: Value = match serde_json::from_str(&payload) {
+        Err(e) => return Box::new(future::err(e.into())),
+        Ok(json) => json,
+    };
+
+    if json["test"].as_bool() == Some(true) {
+        // Bitbucket connection test
+        return Box::new(future::ok("Success"));
+    } else if json["eventKey"].as_str() == Some("pr:opened") {
+        let event: PullRequestOpenedEvent = match serde_json::from_value(json) {
+            Err(e) => return Box::new(future::err(e.into())),
+            Ok(event) => event,
+        };
+        return handle_pr_opened_event(event);
+    } else {
+        return Box::new(future::ok("Ignoring unexpected payload"));
+    }
+}
+
+fn handle_pr_opened_event(
+    event: PullRequestOpenedEvent,
+) -> Box<dyn Future<Item = &'static str, Error = Error>> {
+    let pr = event.pull_request;
+    let base_url = get_base_url(&pr.links.self_link[0].href);
+    let project_key = pr.to_ref.repository.project.key;
+    let repository_slug = pr.to_ref.repository.slug;
+
+    let repo_base_url = format!(
+        "{}rest/api/1.0/projects/{}/repos/{}/",
+        base_url, project_key, repository_slug
+    );
+    let pr_comment_url = format!("{}pull-requests/{}/comments", repo_base_url, pr.id);
+
+    let client = Client::default();
+    let response = client
+        .post(&pr_comment_url)
+        .send_json(&Comment {
+            text: "Test comment".to_string(),
+        })
+        .map_err(|e| e.into())
+        .and_then(|_| Ok("Handled pr:opened event"));
+
+    return Box::new(response);
+}
+
+fn get_base_url(url: &str) -> &str {
+    URL_HOST_REGEX
+        .captures(url)
+        .unwrap()
+        .get(1)
+        .unwrap()
+        .as_str()
 }
---
+++ src/types.rs
@@ -0,0 +1,53 @@
+use serde::{Serialize,Deserialize};
+
+// note: this is obviously only a subset of all the fields that Bitbucket sends.
+// I only implemented those which I need.
+
+#[derive(Deserialize)]
+pub struct PullRequestOpenedEvent {
+    #[serde(rename = "pullRequest")]
+    pub pull_request: PullRequest
+}
+
+#[derive(Deserialize)]
+pub struct PullRequest {
+    pub id: i64,
+    #[serde(rename = "toRef")]
+    pub to_ref: Ref,
+    #[serde(rename = "fromRef")]
+    pub from_ref: Ref,
+    pub links: Links
+}
+
+#[derive(Deserialize)]
+pub struct Links {
+    #[serde(rename = "self")]
+    pub self_link: Vec<Link>
+}
+
+#[derive(Deserialize)]
+pub struct Link {
+    pub href: String
+}
+
+#[derive(Deserialize)]
+pub struct Ref {
+    pub id: String,
+    pub repository: Repository
+}
+
+#[derive(Deserialize)]
+pub struct Repository {
+    pub slug: String,
+    pub project: Project
+}
+
+#[derive(Deserialize)]
+pub struct Project {
+    pub key: String
+}
+
+#[derive(Serialize)]
+pub struct Comment {
+    pub text: String
+}
